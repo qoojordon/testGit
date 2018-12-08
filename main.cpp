4d9d334ba8cf25af0b03c0b406e2d0f9e0bde088
qoojordon <qoojordon@gmail.com>
2018-12-08 21:47:18 +0800

    Revert "Update main.cpp"
    This reverts commit 3a91703ac1587b3b6acbd7363f83c34d621c3fe0.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
 //this is initial decription when this project is created
 //comment for 1st change
 //have some change by github web and commit 2nd time
-//have some change by github web and commit 3rd time
