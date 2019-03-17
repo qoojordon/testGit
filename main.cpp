ec8612094930dc5e6cec0798de45755eafb72493
qoojordon <qoojordon@gmail.com>
2019-03-17 17:42:25 +0800

    modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
 //this is initial decription when this project is created
 //comment for 1st change
 //have some change by github web and commit 2nd time "add a conflict line for test resolve feature"
+
+
+#this is test version for change on Mac
