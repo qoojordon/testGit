28e47c00f4a50ffdbe34190a7441de048e4f96a5
qoojordon <qoojordon@gmail.com>
2021-06-26 13:39:42 +0000

    integrate practice flow into replit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
-//this is initial decription when this project is created
-//comment for 1st change
-//have some change by github web and commit 2nd time "add a conflict line for test resolve feature"
-
-
-#this is test version for change on Mac
+#include <iostream>
+//#include "nd.h"
+using namespace std;
+int main() {
+    std::cout << "Hello, world!";
+    return 0;
+//    ND nd;
+}
